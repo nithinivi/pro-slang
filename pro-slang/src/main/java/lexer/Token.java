497db77754b47049cf6f56cb190f2e5eca1e1cc1
nithinivi <nithinnivi@gmail.com>
2023-05-18 10:26:48 +0530

    tokens for lexer

--- pro-slang/src/main/java/lexer/Token.java
+++ pro-slang/src/main/java/lexer/Token.java
@@ -1,5 +1,9 @@
 package lexer;
 
-public enum Token
-{
+public enum Token {
+    LC_WORD, UC_WORD, NUMERAL,
+    OPEN, CLOSE, SQ_OPEN, SQ_CLOSE,
+    IMPLIED_BY, NOT, AND, COMMA, COLON, DOT, QUESTION,
+    EOF
+
 }
