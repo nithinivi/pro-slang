293861933a8e12bead393f810ee0bed89c3a8b85
nithinivi <nithinnivi@gmail.com>
2023-05-23 01:59:56 +0530

    example

--- pro-slang/src/main/java/ProSlang.java
+++ pro-slang/src/main/java/ProSlang.java
@@ -9,12 +9,13 @@ public class ProSlang {
     public static void main(String[] args) throws Exception {
         String data = """
                                 
-                parent(john, jim).
+                parent(chacko, wilson).
+                parent(wilson, kuttan).
                                 
-                relation(X, Z) <= parent(X, Z).
-                
-                ?relation(john, jim).
-                
+                grandfather(X, Y) <= parent(X, Z) and parent(Z,Y).
+                              
+                ?grandfather(chacko, kuttan).
+                                   
                 """;
         Parser parser = new Parser(data);
         Expression program = parser.parse();
